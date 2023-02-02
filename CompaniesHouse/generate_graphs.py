1b32bd9a64abb5bba59057e9e30cacc88d63f722
Leo <leoellis552@gmail.com>
2023-02-02 11:44:13 +0000

    Graph experimentation

--- CompaniesHouse/generate_graphs.py
+++ CompaniesHouse/generate_graphs.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from CompaniesHouse.CompanySearch import CompanySearch
+from CompaniesHouse.CompanyInfo import CompanyInfo
 
 def preprocessing():
     print()
@@ -7,3 +9,24 @@ def graph():
     plt.plot([1, 2, 3, 4])
     plt.ylabel('some numbers')
     plt.show()
+
+if __name__ == "__main__":
+    company = CompanyInfo('03824658')
+    accountInfo = company.getAccountInformation(2022)
+    turnover = []
+    years = []
+
+    for year in range(2018, 2023):
+        accountInfo = company.getAccountInformation(year)
+
+        for record in accountInfo:
+            if record['name'] == 'ProfitLoss':
+                turnover.append(record['value'])
+                years.append(year)
+                print(turnover, year)
+                break
+
+    plt.plot(years,turnover)
+    plt.ylabel('Profit GBP')
+    plt.show()
+    print(accountInfo)
