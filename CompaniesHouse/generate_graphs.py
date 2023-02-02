4f81301e8ae3bc8bd574c2470788e4536c1d604a
Leo <leoellis552@gmail.com>
2023-02-02 14:27:36 +0000

    Making the graphs prettier

--- CompaniesHouse/generate_graphs.py
+++ CompaniesHouse/generate_graphs.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 from CompaniesHouse.CompanySearch import CompanySearch
 from CompaniesHouse.CompanyInfo import CompanyInfo
+import numpy as np
 
 def preprocessing():
     print()
 
-def graph():
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
+def bar_graph(x,y):
+    menMeans = y
+
+    # Creating a figure with some fig size
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.bar(x, menMeans, width=0.4)
+    # Now the trick is here.
+    # plt.text() , you need to give (x,y) location , where you want to put the numbers,
+    # So here index will give you x pos and data+1 will provide a little gap in y axis.
+    for index, data in enumerate(menMeans):
+        plt.text(x=index, y=data + 1, s=f"{data}", fontdict=dict(fontsize=20))
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
@@ -28,7 +38,14 @@ if __name__ == "__main__":
                     print(extractedData[attribute], year)
                     break
 
-    plt.plot(years,extractedData['ProfitLoss'])
-    plt.ylabel('Profit GBP')
+    plt.bar(years,extractedData['ProfitLoss'])
+    plt.ylabel('Profit in GBP')
+    plt.xlabel('Year')
+
+    plt.ylim([0, max(extractedData['ProfitLoss']) * 1.2])
+
     plt.show()
+
+  #  bar_graph(years,extractedData['ProfitLoss'])
+
     print(accountInfo)
