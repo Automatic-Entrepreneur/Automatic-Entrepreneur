cde6d7bf294c9bb307dd85afc7ed4e963c96cf8c
Justin Lam <jl2261@cam.ac.uk>
2023-02-08 23:44:14 +0000

    Cleaned up the code to make it more compact and easier to maintain

--- CompaniesHouse/performance_summary.py
+++ CompaniesHouse/performance_summary.py
@@ -2,10 +2,19 @@ from CompaniesHouse.CompanySearch import CompanySearch
 from CompaniesHouse.CompanyInfo import CompanyInfo
 
 
-keywords = {"+": "increased", "-": "decreased", "=": "stayed the same"}
+keywords = {1: "increased", -1: "decreased", 0: "stayed the same"}
 
 
-def summary(company_id: str) -> tuple[list[str], list[int]]:
+def ternary(n: int) -> int:
+	if n > 0:
+		return 1
+	elif n < 0:
+		return -1
+	else:
+		return 0
+
+
+def summary(company_id: str) -> tuple[list[int], list[list[int]]]:
 	company = CompanyInfo(company_id)
 	profit_by_year = [
 		(record["value"], year - 1)
@@ -16,35 +25,20 @@ def summary(company_id: str) -> tuple[list[str], list[int]]:
 
 	start = profit_by_year[0]
 	prev = profit_by_year[1]
-	if prev[0] > start:
-		trend = [keywords["+"]]
-	elif prev[0] == start:
-		trend = [keywords["="]]
-	else:
-		trend = [keywords["-"]]
+
+	trend_list = [ternary(prev[0] - start)]
 	groups = [[start[1], prev[1]]]
+
 	for profit, year in profit_by_year[2:]:
-		if trend[-1] == keywords["+"]:
-			if profit > prev[0]:
-				groups[-1] = year
-			else:
-				trend.append(keywords["-"]) if profit < prev[0] else trend.append(keywords["="])
-				groups.append([year, None])
-		elif trend[-1] == keywords["-"]:
-			if profit < prev[0]:
-				groups[-1] = year
-			else:
-				trend.append(keywords["+"]) if profit > prev[0] else trend.append(keywords["="])
-				groups.append([year, None])
+		trend = ternary(profit - prev[0])
+		if trend_list[-1] == trend:
+			groups[-1][-1] = year
 		else:
-			if profit == prev[0]:
-				groups[-1] = year
-			else:
-				trend.append(keywords["+"]) if profit > prev[0] else trend.append(keywords["-"])
-				groups.append([year, None])
+			trend_list.append(trend)
+			groups.append([year, year])
 		prev = (profit, year)
 
-	return trend, groups
+	return trend_list, groups
 
 
 if __name__ == "__main__":
