359bb51c9061c47b7c801af6aafbb37d4a68f5c1
Justin Lam <jl2261@cam.ac.uk>
2023-02-08 23:53:31 +0000

    Added text output

--- CompaniesHouse/performance_summary.py
+++ CompaniesHouse/performance_summary.py
@@ -15,6 +15,7 @@ def ternary(n: int) -> int:
 
 
 def summary(company_id: str) -> tuple[list[int], list[list[int]]]:
+	"""
 	company = CompanyInfo(company_id)
 	profit_by_year = [
 		(record["value"], year - 1)
@@ -22,8 +23,9 @@ def summary(company_id: str) -> tuple[list[int], list[list[int]]]:
 		for record in company.getAccountInformation(year)
 		if record["name"] == "ProfitLoss" and record["startdate"] == f"{year - 1}-01-01"
 	]
+	"""
 
-	# profit_by_year = [(600, 2017), (700, 2018), (100, 2019), (50, 2020), (50, 2021), (500, 2022)]
+	profit_by_year = [(600, 2017), (700, 2018), (100, 2019), (50, 2020), (50, 2021), (500, 2022)]
 
 	start = profit_by_year[0]
 	prev = profit_by_year[1]
@@ -44,4 +46,10 @@ def summary(company_id: str) -> tuple[list[int], list[list[int]]]:
 
 
 if __name__ == "__main__":
-	print(summary("03824658"))
+	trends, intervals = summary("03824658")
+	output = [
+		f"Profit {keywords[trend]} from {start} to {end}."
+		for trend, (start, end) in zip(trends, intervals)
+	]
+	print(" ".join(output))
+
