d26cb409c83d9654615936a5a3f7b9a8ad90869f
hjel <hjel2@cam.ac.uk>
2023-02-28 10:38:56 +0000

    Pushed a change such that news.py looks at the last 30 days

--- news.py
+++ news.py
@@ -3,16 +3,19 @@ try:
 except:
     pass
 from newsapi import NewsApiClient
+from datetime import date, timedelta
 
 def get_news(company):
     newsapi = NewsApiClient(api_key='c818a98d769b414e8df42930ecdd6910')
+    today = date.today().strftime("%Y-%m-%d")
+    last_month = (date.today() - timedelta(days=30)).strftime("%Y-%m-%d")
     all_articles = newsapi.get_everything(q=company.split(' ')[0]+"company",
-                                        from_param='2023-01-27',
-                                        to='2023-02-26',
+                                        from_param=today,
+                                        to=last_month,
                                         language='en',
                                         sort_by='relevancy',
                                         page=2)
-
+    # never fetches anything!
     try:
         sentiment_pipeline = pipeline("sentiment-analysis")
         sentiments = sentiment_pipeline([i["title"] for i in all_articles["articles"][:5]])
