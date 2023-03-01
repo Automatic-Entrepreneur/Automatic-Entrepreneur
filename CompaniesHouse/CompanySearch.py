914260876f331936e5eee10de816c5eefa1719d2
Justin Lam <jl2261@cam.ac.uk>
2023-03-01 16:35:39 +0000

    refactored function names

--- CompaniesHouse/CompanySearch.py
+++ CompaniesHouse/CompanySearch.py
@@ -10,7 +10,8 @@ class TooManyResults(UserWarning):
     """
     To be raised when a search requests more than 500 companies
     """
-    def __init__(self, msg='Number of results for CompanySearch must be ≤ 500'):
+
+    def __init__(self, msg="Number of results for CompanySearch must be ≤ 500"):
         super().__init__(msg)
 
 
@@ -18,12 +19,15 @@ class CompanySearch:
     """
     A class to wrap searches for companies on CompaniesHouse
     """
+
     def __init__(self):
         self.__key = CompaniesHouse.key.api_key
         self.path = os.path.dirname(__file__)
 
     @lru_cache(maxsize=5)
-    def search(self, company_name, active=True, start=0, n=20):
+    def search(
+            self, company_name: str, active: bool = True, start: int = 0, n: int = 20
+    ) -> list[dict[str, str]]:
         """
         Searches query on companies house
         :param company_name: company to search for
@@ -52,35 +56,54 @@ class CompanySearch:
         # return['industry'] a list of strings describing what the company does
         if n > 500:
             raise TooManyResults
-        query = "https://api.company-information.service.gov.uk/advanced-search/companies"
+        query = (
+            "https://api.company-information.service.gov.uk/advanced-search/companies"
+        )
         params = {
-            'company_name_includes': company_name,
-            'start_index': start,
-            'size': n
+            "company_name_includes": company_name,
+            "start_index": start,
+            "size": n,
         }
         if active:
-            params['company_status'] = 'active'
-        results = json.JSONDecoder().decode(requests.get(query, auth=(self.__key, ''), params=params).text)['items']
+            params["company_status"] = "active"
+        results = json.JSONDecoder().decode(
+            requests.get(query, auth=(self.__key, ""), params=params).text
+        )["items"]
         companies = {
-            company['company_number']: {
-                'company_name': company['company_name'],
-                'company_number': company['company_number'],
-                'company_status': company['company_status'],
-                'company_type': company['company_type']
+            company["company_number"]: {
+                "company_name": company["company_name"],
+                "company_number": company["company_number"],
+                "company_status": company["company_status"],
+                "company_type": company["company_type"],
             }
             for company in results
         }
         for company in results:
-            if 'date_of_creation' in company:
-                companies[company['company_number']]['date_of_creation'] = company['date_of_creation']
-            if 'date_of_cessation' in company:
-                companies[company['company_number']]['date_of_cessation'] = company['date_of_cessation']
-            if 'registered_office_address' in company:
-                companies[company['company_number']]['registered_office_address'] = company['registered_office_address']
-            if 'sic_codes' in company:
-                companies[company['company_number']]['sic_codes'] = company['sic_codes']
-        codes_to_text = pkl.load(open(os.path.join(self.path, "sic_codes.pkl"), 'rb'))
+            if "date_of_creation" in company:
+                companies[company["company_number"]]["date_of_creation"] = company[
+                    "date_of_creation"
+                ]
+            if "date_of_cessation" in company:
+                companies[company["company_number"]]["date_of_cessation"] = company[
+                    "date_of_cessation"
+                ]
+            if "registered_office_address" in company:
+                companies[company["company_number"]][
+                    "registered_office_address"
+                ] = company["registered_office_address"]
+            if "sic_codes" in company:
+                companies[company["company_number"]]["sic_codes"] = company["sic_codes"]
+        codes_to_text = pkl.load(open(os.path.join(self.path, "sic_codes.pkl"), "rb"))
         for company in companies.values():
-            if 'sic_codes' in company:
-                company['industry'] = [codes_to_text[sic] for sic in company['sic_codes'] if sic in codes_to_text]
-        return sorted(companies.values(), key=lambda x: x['date_of_creation'] if 'date_of_creation' in x else "9999-99-99")
+            if "sic_codes" in company:
+                company["industry"] = [
+                    codes_to_text[sic]
+                    for sic in company["sic_codes"]
+                    if sic in codes_to_text
+                ]
+        return sorted(
+            companies.values(),
+            key=lambda x: x["date_of_creation"]
+            if "date_of_creation" in x
+            else "9999-99-99",
+        )
