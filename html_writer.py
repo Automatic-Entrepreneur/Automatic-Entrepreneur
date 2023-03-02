15bf3c4aa70ef4fbf414b3a04549cc085b03bb70
Justin Lam <jl2261@cam.ac.uk>
2023-03-02 10:35:25 +0000

    reformatted

--- html_writer.py
+++ html_writer.py
@@ -11,7 +11,17 @@ from templates import *
 
 
 def get_report(company_id, start_year=2010, end_year=2023):
-	name, CH_data, GD_data, CEO_summary, QA_answers, img_paths, captions, news = get_data(company_id)
+	(
+		name,
+		CH_data,
+		GD_data,
+		CEO_summary,
+		QA_answers,
+		img_paths,
+		captions,
+		news,
+	) = get_data(company_id)
+
 	out = ""
 	print(GD_data)
 
@@ -24,7 +34,12 @@ def get_report(company_id, start_year=2010, end_year=2023):
 	out += LOGO
 
 	items = ["Industry", "Size", "Founded", "Overall Rating", "CEO"]
-	out += "".join([(t.format(data=GD_data[i])+"\n") if GD_data[i] != "N/A" else "" for t, i in zip(FACTS, items)])
+	out += "".join(
+		[
+			(t.format(data=GD_data[i]) + "\n") if GD_data[i] != "N/A" else ""
+			for t, i in zip(FACTS, items)
+		]
+	)
 
 	# == BODY ==
 
@@ -34,11 +49,9 @@ def get_report(company_id, start_year=2010, end_year=2023):
 	# = MISSION =
 	if GD_data["Mission"] != "N/A":
 		out += MISSION.format(mission=GD_data["Mission"])
-
 	# = REPORT =
 	if CEO_summary != "":
 		out += REPORT.format(report=CEO_summary, id=company_id)
-
 	# = FINANCE =
 	out += FINANCE_OPEN
 
@@ -50,7 +63,10 @@ def get_report(company_id, start_year=2010, end_year=2023):
 				T = IMAGE_L
 			else:
 				T = IMAGE_R
-			out += T.format(img=path, caption="<p>" + ("</p>\n"+"	"*5+"<p>").join(captions[attribute]) + "</p>")
+			out += T.format(
+				img=path,
+				caption="<p>" + ("</p>\n" + "	" * 5 + "<p>").join(captions[attribute]) + "</p>"
+			)
 		out += GRAPHS_CLOSE
 
 	# STOCKS
@@ -70,35 +86,46 @@ def get_report(company_id, start_year=2010, end_year=2023):
 		out += SATISFACTION_OPEN.format(name=name)
 		out += ""  # satisfaction data (TODO)
 		out += SATISFACTION_CLOSE
-
 	# NEWS
 	if news != "":
 		out += NEWS_OPEN.format(name=name) + news + NEWS_CLOSE
-
 	out += SAYING_CLOSE
 
 	# = FAQS =
 	if len(QA_answers) != 0:
 		out += FAQS_OPEN
 		for i in QA_answers:
-			out += "					<p><b>"+i["q"]+"</b> "+i["a"]+"</p>\n"
+			out += "					<p><b>" + i["q"] + "</b> " + i["a"] + "</p>\n"
 		out += FAQS_CLOSE
-
 	# = SOCIALS =
 	out += SOCIALS_OPEN
 	images = [
-		("Twitter", "https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"),
-		("LinkedIn", "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png"),
-		("Instagram", "https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"),
-		("Facebook", "https://upload.wikimedia.org/wikipedia/commons/f/fb/Facebook_icon_2013.svg"),
-		("YouTube", "https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg")
+		(
+			"Twitter",
+			"https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg",
+		),
+		(
+			"LinkedIn",
+			"https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png",
+		),
+		(
+			"Instagram",
+			"https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg",
+		),
+		(
+			"Facebook",
+			"https://upload.wikimedia.org/wikipedia/commons/f/fb/Facebook_icon_2013.svg",
+		),
+		(
+			"YouTube",
+			"https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg",
+		),
 	]
 	for data, image in images:
 		if GD_data[data] != "N/A":
 			out += SOCIALS.format(data=GD_data[data], image=image)
 	if GD_data["Website"] != "N/A":
 		out += WEBSITE.format(website=GD_data["Website"])
-
 	out += SOCIALS_CLOSE.format(time=datetime.now().strftime("%H:%M %d/%d/%Y"))
 	return out
 
@@ -118,8 +145,9 @@ def get_data(company_id, start_year=2010, end_year=2023, torch=True):
 	QA_answers = answer_question(QA_text, questions, not torch)
 
 	if GD_data["Headquarters"] != "N/A":
-		QA_answers.append({"q": f"Where is {name}'s headquarters?", "a": GD_data["Headquarters"]})
-
+		QA_answers.append(
+			{"q": f"Where is {name}'s headquarters?", "a": GD_data["Headquarters"]}
+		)
 	print("generating graphs")
 	img_paths = generate_bar_graph(
 		CH_data["data"], "static/", company_id, show_graph=False
@@ -138,29 +166,30 @@ def glassdoor_info(company_name):
 	header = Headers(
 		browser="chrome",  # Generate only Chrome UA
 		os="win",  # Generate only Windows platform
-		headers=False  # generate misc headers
+		headers=False,  # generate misc headers
 	)
-	custom_user_agent = header.generate()['User-Agent']
+	custom_user_agent = header.generate()["User-Agent"]
 	chrome_options = webdriver.ChromeOptions()
 	chrome_options.add_experimental_option("useAutomationExtension", False)
 	chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-	chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})  # disables cookies
+	chrome_options.add_experimental_option(
+		"prefs", {"profile.default_content_setting_values.cookies": 2}
+	)  # disables cookies
 	chrome_options.add_argument(f"user-agent={custom_user_agent}")
 
 	# This line prevents the pop-up
 	chrome_options.add_argument("--headless")
-	chrome_options.add_argument('--ignore-certificate-errors')
-	chrome_options.add_argument('--incognito')
+	chrome_options.add_argument("--ignore-certificate-errors")
+	chrome_options.add_argument("--incognito")
 
 	driver = webdriver.Chrome(options=chrome_options)
 
 	ret = dict()
-	
+
 	glassdoor_scrape(driver, company_name, ret)
 
-	if ret['Ticker'] != 'N/A':
-		finance_scrape(ret['Ticker'], ret)
-
+	if ret["Ticker"] != "N/A":
+		finance_scrape(ret["Ticker"], ret)
 	get_socials(driver, ret)
 	return ret
 
