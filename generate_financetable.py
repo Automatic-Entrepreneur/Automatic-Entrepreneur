c78f61270821542cf5f6152b7de9b9fd0102a6bb
Ke-Ming <kmc64@cam.ac.uk>
2023-03-07 16:14:27 +0000

    fixed issues with generate_table

--- generate_financetable.py
+++ generate_financetable.py
@@ -3,103 +3,92 @@ import json
 
 
 def generate_table(GD_data):
-    bs_keys = ['totalAssets',
-      'investments', 
-      'totalLiabilities', 
-      'deferredRevenue',
-      'currentDebt']
-    is_keys = ['researchAndDevelopment', 
-            'ebitda', 
-            'grossProfit', 
-            'totalRevenue', 
-            'netIncome']
-    cf_keys = ['operatingCashflow', 
-            'profitLoss']
+bs_keys =  ['totalAssets',
+            'investments', 
+            'totalLiabilities', 
+            'deferredRevenue',
+            'currentDebt']
+is_keys = ['researchAndDevelopment', 
+           'ebitda', 
+           'grossProfit', 
+           'totalRevenue', 
+           'netIncome']
+cf_keys = ['operatingCashflow', 
+           'profitLoss']
 
-    return_df = pd.DataFrame()
-        
-    for i in ['Balance Sheet', 'Income Statement', 'Cash Flow', 'Annual Earnings']:
-        x = pd.DataFrame(GD_data[i])
-        '''res = [json.loads(idx.replace("'", '"')) for idx in [x]]
-        for j in res:
-            newlist = j
-        x = pd.DataFrame(newlist)'''
-        if i == 'Balance Sheet':
-            lst = []
-            for d in GD_data[i]:
-                dic = {key: d[key] for key in (['fiscalDateEnding'] 
-                                               + bs_keys)}
-                for k, v in dic.items():
-                        if v == 'None':
-                            dic[k] = 0
-                dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in bs_keys)
-                lst.append(dic)
-            x = pd.DataFrame(lst)
-        elif i == 'Income Statement':
-            lst = []
-            for d in GD_data[i]:
-                dic = {key: d[key] for key in (['fiscalDateEnding',
-                                               'reportedCurrency'] 
-                                               + is_keys)}
-                for k, v in dic.items():
-                        if v == 'None':
-                            dic[k] = 0
-                dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in is_keys)
-                lst.append(dic)
-            x = pd.DataFrame(lst)
-        elif i == 'Cash Flow':
-            lst = []
-            for d in GD_data[i]:
-                dic = {key: d[key] for key in (['fiscalDateEnding'] 
-                                                + cf_keys)}
-                for k, v in dic.items():
-                        if v == 'None':
-                            #print(k)
-                            dic[k] = 0
-                dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in cf_keys)
-                lst.append(dic)
-            x = pd.DataFrame(lst)
-        elif i == 'Annual Earnings':
-            count = 0
-            lst = []
-            for d in GD_data[i]:
-                if count < 5:
-                    for k, v in d.items():
-                            if v == 'None':
-                                #print(k)
-                                d[k] = 0
-                    count += 1
-                    lst.append(d)
-            x = pd.DataFrame(lst)
-            
-        # Set datetime
-        x['fiscalDateEnding'] = pd.to_datetime(x['fiscalDateEnding'])
-        
-        # Merge dataframes
-        if return_df.empty:
-            return_df = x
-        else:
-            return_df = return_df.merge(x, on = 'fiscalDateEnding')
-            
+    lst = []
+    for di in GD_data['Balance Sheet']:
+        dic = {key: di[key] for key in (['fiscalDateEnding'] 
+                                       + bs_keys)}
+        for k, v in dic.items():
+                if v == 'None':
+                    dic[k] = 0
+        dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in bs_keys)
+        lst.append(dic)
+    bs_df = pd.DataFrame(lst)
+
+    lst = []
+    for di in GD_data['Income Statement']:
+        dic = {key: di[key] for key in (['fiscalDateEnding',
+                                       'reportedCurrency'] 
+                                       + is_keys)}
+        for k, v in dic.items():
+                if v == 'None':
+                    dic[k] = 0
+        dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in is_keys)
+        lst.append(dic)
+    is_df = pd.DataFrame(lst)
+
+    lst = []
+    for di in GD_data['Cash Flow']:
+        dic = {key: di[key] for key in (['fiscalDateEnding'] 
+                                        + cf_keys)}
+        for k, v in dic.items():
+                if v == 'None':
+                    dic[k] = 0
+        dic.update((k, int(dic[k])/1000000) for k, v in dic.items() if k in cf_keys)
+        lst.append(dic)
+    cf_df = pd.DataFrame(lst)
+
+    count = 0
+    lst = []
+    for di in GD_data['Annual Earnings']:
+        if count < 5:
+            for k, v in di.items():
+                    if v == 'None':
+                        di[k] = 0
+            count += 1
+            lst.append(di)
+    ae_df = pd.DataFrame(lst)
+
+    # Set datetime
+    bs_df['fiscalDateEnding'] = pd.to_datetime(bs_df['fiscalDateEnding'])
+    is_df['fiscalDateEnding'] = pd.to_datetime(is_df['fiscalDateEnding'])
+    cf_df['fiscalDateEnding'] = pd.to_datetime(cf_df['fiscalDateEnding'])
+    ae_df['fiscalDateEnding'] = pd.to_datetime(ae_df['fiscalDateEnding'])
+
+    # Merge dataframes
+    return_df = bs_df.merge(is_df, on = 'fiscalDateEnding')
+    return_df = return_df.merge(cf_df, on = 'fiscalDateEnding')
+    return_df = return_df.merge(ae_df, on = 'fiscalDateEnding')
+
     # Rename Columns
     return_df = return_df.rename(columns = {'fiscalDateEnding' : 'FY', 
-                                'reportedEPS' : 'EPS', 
-                                'grossProfit' : 'Gross Profit (M)', 
-                                'totalRevenue' : 'Revenue (M)', 
-                                'ebitda' : 'EBITDA (M)', 
-                                'netIncome' : 'Net Income (M)', 
-                                'totalAssets' : 'Assets (M)', 
-                                'investments' : 'Investments (M)', 
-                                'totalLiabilities' : 'Liabilities (M)', 
-                                'deferredRevenue' : 'Deferred Revenue (M)', 
-                                'currentDebt' : 'Current Debt (M)', 
-                                'operatingCashflow' : 'Operating Cash Flow (M)', 
-                                'profitLoss' : 'Profit Loss (M)', 
-                                'reportedCurrency' : 'Currency', 
-                                'researchAndDevelopment' : 'R&D (M)'})
-    # temporarily set None to 0
-    #return_df = return_df.replace('None', 0)
-
+                                  'reportedEPS' : 'EPS', 
+                                  'grossProfit' : 'Gross Profit (M)', 
+                                  'totalRevenue' : 'Revenue (M)', 
+                                  'ebitda' : 'EBITDA (M)', 
+                                  'netIncome' : 'Net Income (M)', 
+                                  'totalAssets' : 'Assets (M)', 
+                                  'investments' : 'Investments (M)', 
+                                  'totalLiabilities' : 'Liabilities (M)', 
+                                  'deferredRevenue' : 'Deferred Revenue (M)', 
+                                  'currentDebt' : 'Current Debt (M)', 
+                                  'operatingCashflow' : 'Operating Cash Flow (M)', 
+                                  'profitLoss' : 'Profit Loss (M)', 
+                                  'reportedCurrency' : 'Currency', 
+                                  'researchAndDevelopment' : 'R&D (M)'})
+    
     # set financial year
     return_df['FY'] = pd.DatetimeIndex(return_df['FY']).year
 
@@ -119,13 +108,8 @@ def generate_table(GD_data):
     return_df['EPS'] = return_df['EPS'].astype(float)
     pd.options.display.float_format = '{:,.2f}'.format
     return_df['EPS'] = return_df['EPS'].astype(str)
+   
 
-    '''
-    # convert to millions
-    for i in mils:
-        return_df[i] = return_df[i]/1000000
-        return_df[[i]] = return_df[[i]].astype(int)'''
-        
     # replace 0s
     return_df = return_df.replace(0, 'N/A')
 
